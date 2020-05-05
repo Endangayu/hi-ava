35f1e753d63a0fdd09089331c1c9b5d69926ed53
Endangayu <64842707+Endangayu@users.noreply.github.com>
2020-05-05 20:36:15 +0700

    jjjj
    yaaaaaay
    
    Co-Authored-By: endangayu <endangayu@users.noreply.github.com>

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
+  nicknames: ["Apiiip", "My Bootleg", "Rafiif", "Apiiip", "Apiip"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
